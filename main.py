474fb8f91cb9a1651b9f4998b7206d93237d27bf
Deacon Williams <deacon.williams99@gmail.com>
2026-04-11 09:17:30 -0500

    Was having issues with user loop, took out computer loop till i got user loop fixed

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import random
 
 print(art.logo)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-user_cards = []
-computer_cards = []
+
 user_score = 0
 computer_score = 0
 
@@ -20,36 +19,47 @@ def game_over (current_cards, current_score, current_computer_cards, current_com
     print(f"Computers final cards: {current_computer_cards}, final hand: {current_computer_score}")
 
 user_continues = True
-computer_continues = True
 blackjack = True
 while blackjack:
+    user_cards = []
+    computer_cards = []
+    user_continues = True
+
     user_cards.append(get_cards())
     user_cards.append(get_cards())
     computer_cards.append(get_cards())
     user_score = sum(user_cards)
     computer_score = sum(computer_cards)
     current_game(current_cards=user_cards, current_score=user_score, current_computer_cards=computer_cards)
+
     get_new_card = input("Press 'y' to get another card or 'n' to pass: ")
     if get_new_card == 'y':
         while user_continues:
             new_card = user_cards.append(get_cards())
             user_score = sum(user_cards)
             current_game(current_cards=user_cards, current_score=user_score, current_computer_cards=computer_cards)
+            if user_score > 21:
+                print("You Lose")
+                user_continues = False
+                continue
+
             get_new_card = input("Press 'y' to get another card or 'n' to pass: ")
             if get_new_card == 'n':
                 user_continues = False
-            else:
-                user_continues = True
 
-
-    while computer_continues:
-        new_computer_card = computer_cards.append(get_cards())
-        computer_score = sum(computer_cards)
-        if computer_score <= 16:
-            computer_continues = True
-        else:
-            computer_continues = False
-            game_over(current_cards = user_cards, current_score=user_score, current_computer_cards=computer_cards, current_computer_score=computer_score)
-    print("\n" * 20)
+    continue_game = input("Press 'y' to play a game of Blackjack or 'n' to quit: ")
+    if continue_game == 'n':
+        blackjack = False
+    else:
+        print("\n" * 20)
+    # while computer_continues:
+    #     new_computer_card = computer_cards.append(get_cards())
+    #     computer_score = sum(computer_cards)
+    #     if computer_score <= 16:
+    #         computer_continues = True
+    #     else:
+    #         computer_continues = False
+    #         game_over(current_cards = user_cards, current_score=user_score, current_computer_cards=computer_cards, current_computer_score=computer_score)
+    # print("\n" * 20)
 
 
