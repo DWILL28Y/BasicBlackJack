79de9b74e627de550e02f8e4d0266cfbb4435278
Deacon Williams <deacon.williams99@gmail.com>
2026-04-10 22:38:24 -0500

    Fixed while loops, added some more essential code

--- main.py
+++ main.py
@@ -8,49 +8,48 @@ computer_cards = []
 user_score = 0
 computer_score = 0
 
-def get_user_cards ():
-    user_new_card = random.choice(cards)
-    return user_new_card
-
-def get_computer_cards ():
-    computer_new_card = random.choice(cards)
-    return computer_new_card
+def get_cards ():
+    return random.choice(cards)
 
 def current_game(current_cards, current_score, current_computer_cards):
     print(f"Your cards are: {current_cards}, current hand: {current_score}")
     print(f"Computers first card: {current_computer_cards}")
 
-def game_over (current_cards, current_score, current_computer_cards):
+def game_over (current_cards, current_score, current_computer_cards, current_computer_score):
     print(f"Your final cards are: {current_cards}, final hand: {current_score}")
-    print(f"Computers final cards: {current_computer_cards}")
+    print(f"Computers final cards: {current_computer_cards}, final hand: {current_computer_score}")
 
 user_continues = True
 computer_continues = True
 blackjack = True
 while blackjack:
-    user_cards.append(get_user_cards())
-    user_cards.append(get_user_cards())
-    computer_cards.append(get_computer_cards())
-    user_score += sum(user_cards)
-
+    user_cards.append(get_cards())
+    user_cards.append(get_cards())
+    computer_cards.append(get_cards())
+    user_score = sum(user_cards)
+    computer_score = sum(computer_cards)
     current_game(current_cards=user_cards, current_score=user_score, current_computer_cards=computer_cards)
-    while user_continues:
-        get_new_card = input("Press 'y' to get another card or 'n' to pass: ")
-        if get_new_card == 'y':
-            user_cards.append(get_user_cards())
-            user_score += sum(user_cards)
+    get_new_card = input("Press 'y' to get another card or 'n' to pass: ")
+    if get_new_card == 'y':
+        while user_continues:
+            new_card = user_cards.append(get_cards())
+            user_score = sum(user_cards)
             current_game(current_cards=user_cards, current_score=user_score, current_computer_cards=computer_cards)
-        else:
-            user_continues = False
-
-    else:
-        while computer_continues:
-            computer_cards.append(get_computer_cards())
-            computer_score += sum(computer_cards)
-            if computer_score <= 16:
-                computer_continues = True
+            get_new_card = input("Press 'y' to get another card or 'n' to pass: ")
+            if get_new_card == 'n':
+                user_continues = False
             else:
-                game_over(current_cards = user_cards, current_score=user_score, current_computer_cards=computer_cards)
+                user_continues = True
 
+
+    while computer_continues:
+        new_computer_card = computer_cards.append(get_cards())
+        computer_score = sum(computer_cards)
+        if computer_score <= 16:
+            computer_continues = True
+        else:
+            computer_continues = False
+            game_over(current_cards = user_cards, current_score=user_score, current_computer_cards=computer_cards, current_computer_score=computer_score)
+    print("\n" * 20)
 
 
